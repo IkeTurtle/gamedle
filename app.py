5c312e810923d5c2edc71d790c13a6be08d31eba
Quang Anh Nguyen <s_nguyenq21@stud.hwr-berlin.de>
2024-12-05 17:43:45 +0100

    Displaying the sample data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 import db
 
 app = Flask(__name__)
@@ -14,6 +14,41 @@ app.teardown_appcontext(db.close_db_con)
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/GameSet<int:game_id>/<int:round>')
+def game_set(game_id, round):
+    db_con = db.get_db_con()
+
+    # Abfrage des GameSets
+    game_set = db_con.execute(
+        "SELECT * FROM GameSet WHERE id = ?",
+        (game_id,)
+    ).fetchone()
+
+    if not game_set:
+        return "GameSet not found", 404
+
+    # Entnahme der ID vom GuessingObject
+    object_id = game_set[f'object_{round}']  
+    if not object_id:
+        return f"No object for round {round}", 404
+
+    # Abfrage des spezifischen GuessingObjects
+    guessing_object = db_con.execute(
+        "SELECT * FROM GuessingObject WHERE id = ?",
+        (object_id,)
+    ).fetchone()
+
+    if not guessing_object:
+        return "GuessingObject not found", 404
+
+    # HTML-Template rendern
+    return render_template("game_set.html", guessing_object=guessing_object, game_id=game_id, round=round)
+
+ # Wenn /GameSet1 geöffnet wird, wird automatisch /GameSet1/1 geöffnet
+@app.route('/GameSet<int:game_id>')
+def game_set_start(game_id):
+    return redirect(url_for('game_set', game_id=game_id, round=1))
 
 @app.route('/insert/sample')
 def run_insert_sample():
