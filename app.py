6fae37781084e988c19fe6bb5d0954c1eb765df6
Quang Anh Nguyen <s_nguyenq21@stud.hwr-berlin.de>
2025-02-10 18:44:09 +0100

    saving score through rounds with "Flask session"

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap5
 import db
 
 app = Flask(__name__)
+app.secret_key = 'super_secret_key' 
+app.config['SESSION_PERMANENT'] = False
+
 bootstrap = Bootstrap5(app)
 
 app.config.from_mapping(
@@ -59,16 +62,59 @@ def game_set(game_id, round):
 
     if not guessing_object:
         return "GuessingObject not found", 404
+    
+    # Session initialisieren
+    if 'score' not in session:
+        session['score'] = 0
+        session['current_round'] = 1
+
+        print(f"User input: { session['score']}")  
+        print(f"User input: { session['current_round']}")  
+
+    if round != session['current_round']:
+        return redirect(f"/GameSet{game_id}/{session['current_round']}")
+
 
     # https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request?utm_source=chatgpt.com
     if request.method == "POST":
         user_guess = request.form.get("guess", type=float)  
+
+
         print(f"User input: {user_guess}")  
 
+        if session['current_round'] < 10:
+
+            if user_guess is not None:
+                session['score'] += user_guess  # Wert addieren
+                session['current_round'] += 1   # Runde erhöhen
+
+                print(f"Score: { session['score']}")  
+                print(f"Current Roud: { session['current_round']}")  
+
+                return redirect(f"/GameSet{game_id}/{session['current_round']}")
+
+        if session['current_round'] == 10:
+             return redirect(f"/GameSet{game_id}/score")
+
+
     # HTML-Template rendern
-    return render_template("game_set.html", guessing_object=guessing_object, game_id=game_id, round=round, game_name=game_set['name'])
+    return render_template("game_set.html", guessing_object=guessing_object, game_id=game_id, game_name=game_set['name'], score=session['score'], round=session['current_round'])
 
+@app.route('/GameSet<int:game_id>/score')
 
+def scorelist(game_id):
+    db_con = db.get_db_con()
+
+    # Abfrage des GameSets
+    game_set = db_con.execute(
+        "SELECT * FROM GameSet WHERE id = ?",
+        (game_id,)
+    ).fetchone()
+
+    scorepoints = db_con.execute("SELECT username, points FROM Score WHERE game_id = {game_id}").fetchall()
+
+    # Rendern des Templates und Übergabe der GameSets
+    return render_template('game_score.html', games=games)
 
 @app.route('/GameSet<int:game_id>')
 def game_set_start(game_id):
