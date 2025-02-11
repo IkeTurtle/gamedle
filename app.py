1d191a48a23a8bddac15fbc925a988f7597594f7
Quang Anh Nguyen <s_nguyenq21@stud.hwr-berlin.de>
2025-02-11 15:24:07 +0100

    Saving and displaying scores

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ def home():
     games = db_con.execute("SELECT id, name FROM GameSet").fetchall()
 
     # Rendern des Templates und Übergabe der GameSets
+
+    session.clear()
+
     return render_template('home.html', games=games)
 
 @app.route('/games')
@@ -35,14 +38,6 @@ def game_score():
 
     # Rendern des Templates und Übergabe der Game Selection
     return render_template('game_select.html')
-
-@app.route('/Highscores')
-def games():
-    db_con = db.get_db_con()
-
-
-    # Rendern des Templates und Übergabe der Game Selection
-    return render_template('game_score.html')
 
 @app.route('/GameSet<int:game_id>/<int:round>',methods=['GET', 'POST'])
 def game_set(game_id, round):
@@ -71,6 +66,10 @@ def game_set(game_id, round):
     if not guessing_object:
         return "GuessingObject not found", 404
     
+    #Falls ein unlässiges GameObject geöffnet wird
+    if round > 10:
+        return redirect(f"/GameSet{game_id}/1")
+
     # Session initialisieren
     if 'score' not in session:
         session['score'] = 0
@@ -79,6 +78,7 @@ def game_set(game_id, round):
         print(f"User input: { session['score']}")  
         print(f"User input: { session['current_round']}")  
 
+    #Wenn eine man ein anderes GameObject öffnet, obwohl man das aktuelle noch nicht beantwortet hat
     if round != session['current_round']:
         return redirect(f"/GameSet{game_id}/{session['current_round']}")
 
@@ -88,28 +88,40 @@ def game_set(game_id, round):
         user_guess = request.form.get("guess", type=float)  
 
 
-        print(f"User input: {user_guess}")  
-
         if session['current_round'] < 10:
 
             if user_guess is not None:
                 session['score'] += user_guess  # Wert addieren
                 session['current_round'] += 1   # Runde erhöhen
 
+                print(f"User input: {user_guess}")  
                 print(f"Score: { session['score']}")  
                 print(f"Current Roud: { session['current_round']}")  
 
                 return redirect(f"/GameSet{game_id}/{session['current_round']}")
 
+        #Weiterleitung zur Scoresliste
         if session['current_round'] == 10:
-             return redirect(f"/GameSet{game_id}/score")
 
+            session['score'] += user_guess
+
+            print(f"User input: {user_guess}")  
+            print(f"Score: { session['score']}")  
+            print(f"Current Roud: { session['current_round']}")  
+            
+            final_score = session['score']
+
+
+            return redirect(f"/GameSet{game_id}/score")
+
+            
+        
 
     # HTML-Template rendern
     return render_template("game_set.html", guessing_object=guessing_object, game_id=game_id, game_name=game_set['name'], score=session['score'], round=session['current_round'])
 
-@app.route('/GameSet<int:game_id>/score')
 
+@app.route('/GameSet<int:game_id>/score',methods=['GET', 'POST'])
 
 def scorelist(game_id):
     db_con = db.get_db_con()
@@ -120,10 +132,38 @@ def scorelist(game_id):
         (game_id,)
     ).fetchone()
 
-    scorepoints = db_con.execute("SELECT username, points FROM Score WHERE game_id = {game_id}").fetchall()
+    if not game_set:
+        return "GameSet not found", 404
+
+    #Falls man sich noch im Spiel befindet, soll man das Spiel zuerst zuende spielen
+    if session.get('current_round') and session.get('current_round') > 10:
+        return redirect(f"/GameSet{game_id}/{session.get('current_round')}")
+
+
+    game_set = db_con.execute("SELECT * FROM GameSet WHERE id = ?",(game_id,)).fetchone()
+    
+    score_data = db_con.execute("SELECT username, points FROM Score WHERE game_id = ? ORDER BY points DESC", (game_id,)).fetchall()
+
+    final_score = session.get('score')
+
+    #Button, damit Score gespeichert wird
+    if request.method == "POST":
+        username = request.form.get("yourName")
+
+        print(game_id)
+        print(username)
+        print(final_score)
+
+        db_con.execute("INSERT INTO Score (game_id, username, points) VALUES (?, ?, ?)", (game_id, username, final_score))
+        db_con.commit()
+    
+        session.clear()
+        
+        return redirect(f"/GameSet{game_id}/score")
+
 
     # Rendern des Templates und Übergabe der GameSets
-    return render_template('game_score.html', games=games)
+    return render_template('game_score.html', game_id=game_id, game_set=game_set, score_data = score_data, final_score = final_score )
 
 
 @app.route('/GameSet<int:game_id>')
